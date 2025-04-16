f85ccf1fabb37d34e6bae4c58c59763a3c02eeed
PhoenixFury0000 <piyushguptaofficial07042009@gmail.com>
2025-04-16 23:19:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'Yūrei Bot𖤍',
+  author: 'tony stark',
+  botName: "Yūrei Bot𖤍",
+  botOwner: 'PhøenixFury', // Your name
+  ownerNumber: '917973456275', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
